8ca634b0ab485dbcaee9bb0c63eb53086be58f36
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-05-07 09:24:22 -0400

    added basic inquirer prompts and generating initial html file

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,29 @@
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+    const e = new Employee(),
+    expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via constructor arguments", () => {
+    // Assemble
+    const name = "Alice";
+    // Act
+    const e = new Employee(name);
+    // Assertion
+    expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+
